67720abc7c4e09fc88f4bb4a77ee111e5a6c13d2
jiaxiyang <jiaxiyangbit@126.com>
2024-01-27 18:41:16 +0800

    add my own gemm

--- gpu/cuda/run.sh
+++ gpu/cuda/run.sh
@@ -2,8 +2,11 @@ set -ex
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 export CUDA_VISIBLE_DEVICES=7
-/usr/local/cuda/bin/nvcc -O3 -o cuda_sgemm cuda_sgemm.cu -lcublas
-./cuda_sgemm
-python3 plot_bar.py cuda_sgemm.csv cuda_sgemm.png
-# /usr/local/cuda/bin/nvcc -O3 -o matmul matmul.cu -lcublas
-# ./matmul
+# /usr/local/cuda/bin/nvcc --ptxas-options=-v -O3 -o cuda_sgemm cuda_sgemm.cu -lcublas
+# ./cuda_sgemm
+# python3 plot_bar.py cuda_sgemm.csv cuda_sgemm.png
+
+# /usr/local/cuda/bin/nvcc --resource-usage -O3 -o my_sgemm my_sgemm.cu -lcublas  2>&1 | c++filt 
+/usr/local/cuda/bin/nvcc --ptxas-options=-v -O3 -o my_sgemm my_sgemm.cu -lcublas
+./my_sgemm
+python3 plot_bar.py my_sgemm.csv my_sgemm.png
